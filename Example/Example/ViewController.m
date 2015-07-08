88ff21273811775c7f54a337f9aab2c98acd18bf
Marcelo Perretta <mperretta@e-commla.com>
2015-07-08 20:25:26 -0300

    Updated coachmarks position

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -24,36 +24,25 @@
 #pragma mark - Tutorial
 -(void) showTutorial{
     
-    CGRect coachmark1 = CGRectMake(0, 120, 125, 125);
-    CGRect coachmark2 = CGRectMake(52, 318, 60, 60);
+    CGRect coachmark1 = CGRectMake(([UIScreen mainScreen].bounds.size.width - 125) / 2, 64, 125, 125);
+    CGRect coachmark2 = CGRectMake(([UIScreen mainScreen].bounds.size.width - 300) / 2, coachmark1.origin.y + coachmark1.size.height, 300, 80);
     CGRect coachmark3 = CGRectMake(2, 20, 45, 45);
-    
-    CGFloat screenSize = [UIScreen mainScreen].bounds.size.width;
-    if(screenSize >= 375 && screenSize < 414){ //@2x
-        coachmark1 = CGRectMake(28, 120, 125, 125);
-        coachmark2 = CGRectMake(80, 318, 60, 60);
-        coachmark3 = CGRectMake(4, 20, 45, 45);
-    } else if(screenSize >= 414){ //@3x
-        coachmark1 = CGRectMake(48, 120, 125, 125);
-        coachmark2 = CGRectMake(100, 318, 60, 60);
-        coachmark3 = CGRectMake(8, 20, 45, 45);
-    }
     
     // Setup coach marks
     NSArray *coachMarks = @[
                             @{
                                 @"rect": [NSValue valueWithCGRect:coachmark1],
                                 @"caption": @"You can put marks over images",
-                                @"shape": @"rect"
-                                },
-                            @{
-                                @"rect": [NSValue valueWithCGRect:coachmark2],
-                                @"caption": @"Also, we can show buttons",
                                 @"shape": @"circle"
                                 },
                             @{
+                                @"rect": [NSValue valueWithCGRect:coachmark2],
+                                @"caption": @"Also, we can show buttons"
+                                },
+                            @{
                                 @"rect": [NSValue valueWithCGRect:coachmark3],
-                                @"caption": @"And works with navigations buttons too"
+                                @"caption": @"And works with navigations buttons too",
+                                @"shape": @"rect"
                                 }
                             ];
     
